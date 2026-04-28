437c4cc0766e99a4420b13c56bcc046a3c732093
mann gadhiya <ixinemann@gmail.com>
2026-04-28 23:28:19 +0530

    Add files via upload

---
+++ PR-4.php
@@ -0,0 +1,54 @@
+<?PHP
+$x = "10";
+$y = 5;
+echo $x + $y;
+?>
+
+<?php
+$a = "5 apples";
+$b = 10;
+echo $a + $b;
+?>
+
+
+<?php
+$price = 99.99;
+echo (int)$price;
+?>
+
+<?php
+$x = true;
+$y = false;
+echo $x + $y;
+?>
+
+<?php
+echo "5" * 2;
+?>
+
+<?php
+$x = "10";
+$y = 5;
+echo $x + $y;
+?>
+
+<?php
+$x = 10.5;
+$y = "abc";
+echo $x + $y;
+?>
+
+<?php
+$flag = "false";
+if ($flag) {
+ echo "True";
+}
+?>
+
+<?php
+if ("10" == 10)
+?>
+
+<?php
+$x = 5 + "5 days";
+?>
